6862a13d3f2a746c119e8cc18d63f71fa0f6067a
Morten <friesgaard@gmail.com>
2014-03-15 21:23:12 +0100

    MoveView moved to separate class

--- src/dk/eightyplus/Painter/FingerPaint.java
+++ src/dk/eightyplus/Painter/FingerPaint.java
@@ -33,7 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class FingerPaint extends GraphicsActivity implements ColorPickerDialog.OnColorChangedListener {
+public class FingerPaint extends GraphicsActivity implements ColorPickerDialog.OnColorChangedListener, MoveView.MoveCallBack {
 
   private final List<Component> components = new ArrayList<Component>();
   private final Stack<Undo> undo = new Stack<Undo>();
@@ -116,6 +116,18 @@ public class FingerPaint extends GraphicsActivity implements ColorPickerDialog.O
   public void colorChanged(int color) {
     view.color = color;
     mPaint.setColor(color);
+  }
+
+  @Override
+  public void move(Component component, float dx, float dy) {
+    RectF bounds = component.getBounds();
+    undo.add(new Undo(component, bounds.left, bounds.top, State.Move));
+    component.move(dx, dy);
+
+    components.add(component);
+    layout.removeView(moveView);
+    moveView = null;
+    redraw();
   }
 
   public class MyView extends View {
@@ -258,7 +270,7 @@ public class FingerPaint extends GraphicsActivity implements ColorPickerDialog.O
               redraw();
 
               //Component path = getLastPath();
-              moveView = new MoveView(getApplicationContext(), moveComponent);
+              moveView = new MoveView(getApplicationContext(), moveComponent, FingerPaint.this);
               layout.addView(moveView);
               moveView.onTouchEvent(event);
             }
@@ -484,7 +496,7 @@ public class FingerPaint extends GraphicsActivity implements ColorPickerDialog.O
 
       case MOVE_MENU_ID:
         Component path = getLastPath();
-        moveView = new MoveView(getApplicationContext(), path);
+        moveView = new MoveView(getApplicationContext(), path, this);
         layout.addView(moveView);
         return true;
     }
@@ -570,88 +582,7 @@ public class FingerPaint extends GraphicsActivity implements ColorPickerDialog.O
   }
 
 
-  private class MoveView extends View {
 
-
-
-    private int _xDelta;
-    private int _yDelta;
-    private Bitmap mBitmap;
-    private Component component;
-    private Paint   mBitmapPaint;
-    private Paint mPaint;
-
-    public MoveView(Context context, Component component) {
-      super(context);
-      this.component = component;
-
-      mPaint = new Paint();
-      mPaint.setAntiAlias(true);
-      mPaint.setDither(true);
-      mPaint.setStyle(Paint.Style.STROKE);
-      mPaint.setStrokeJoin(Paint.Join.ROUND);
-      mPaint.setStrokeCap(Paint.Cap.ROUND);
-      mPaint.setStrokeWidth(12);
-      setLayerType(LAYER_TYPE_HARDWARE, mPaint); // TODO API level 11
-      mBitmapPaint = new Paint(Paint.DITHER_FLAG);
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-      super.onSizeChanged(w, h, oldw, oldh);
-      mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888); // TODO create smaller bitmap (from bounds)
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-      canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
-      component.onDraw(canvas, mPaint);
-
-      RectF bounds = component.getBounds();
-
-      mPaint.setColor(0xFF000000);
-      mPaint.setStrokeWidth(1.0f);
-      mPaint.setPathEffect(new DashPathEffect(new float[] {10,20}, 0));
-      canvas.drawRoundRect(bounds, 5.0f, 5.0f, mPaint);
-      mPaint.setPathEffect(null);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-      final int X = (int) event.getRawX();
-      final int Y = (int) event.getRawY();
-      switch (event.getAction() & MotionEvent.ACTION_MASK) {
-        case MotionEvent.ACTION_DOWN:
-          RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) moveView.getLayoutParams();
-          _xDelta = X - lParams.leftMargin;
-          _yDelta = Y - lParams.topMargin;
-          break;
-        case MotionEvent.ACTION_UP:
-          RectF bounds = component.getBounds();
-          undo.add(new Undo(component, bounds.left, bounds.top, State.Move));
-          component.move(X - _xDelta, Y - _yDelta);
-
-          components.add(component);
-          layout.removeView(moveView);
-          moveView = null;
-          redraw();
-          break;
-        case MotionEvent.ACTION_POINTER_DOWN:
-          break;
-        case MotionEvent.ACTION_POINTER_UP:
-          break;
-        case MotionEvent.ACTION_MOVE:
-          RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) moveView.getLayoutParams();
-          layoutParams.leftMargin = X - _xDelta;
-          layoutParams.topMargin = Y - _yDelta;
-          layoutParams.rightMargin = -250;
-          layoutParams.bottomMargin = -250;
-          moveView.setLayoutParams(layoutParams);
-          break;
-      }
-      return true;
-    }
-  };
 
   private static File writeToFile(final Context context, final Bitmap bitmap) throws IOException {
     File file
---
+++ src/dk/eightyplus/Painter/MoveView.java
@@ -0,0 +1,92 @@
+package dk.eightyplus.Painter;
+
+import android.content.Context;
+import android.graphics.*;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+/**
+ * MoveView to put a single component into and show moves by user
+ */
+public class MoveView extends View {
+  private int _xDelta;
+  private int _yDelta;
+  private Bitmap mBitmap;
+  private Component component;
+  private Paint mBitmapPaint;
+  private Paint mPaint;
+
+  private MoveCallBack callBack;
+
+  public interface MoveCallBack {
+    public void move(Component component, float dx, float dy);
+  }
+
+  public MoveView(final Context context, final Component component, final MoveCallBack callBack) {
+    super(context);
+    this.component = component;
+    this.callBack = callBack;
+
+    mPaint = new Paint();
+    mPaint.setAntiAlias(true);
+    mPaint.setDither(true);
+    mPaint.setStyle(Paint.Style.STROKE);
+    mPaint.setStrokeJoin(Paint.Join.ROUND);
+    mPaint.setStrokeCap(Paint.Cap.ROUND);
+    mPaint.setStrokeWidth(12);
+    setLayerType(LAYER_TYPE_HARDWARE, mPaint); // TODO API level 11
+    mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888); // TODO create smaller bitmap (from bounds)
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+    component.onDraw(canvas, mPaint);
+
+    RectF bounds = component.getBounds();
+
+    mPaint.setColor(0xFF000000);
+    mPaint.setStrokeWidth(1.0f);
+    mPaint.setPathEffect(new DashPathEffect(new float[] {10,20}, 0));
+    canvas.drawRoundRect(bounds, 5.0f, 5.0f, mPaint);
+    mPaint.setPathEffect(null);
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    final int X = (int) event.getRawX();
+    final int Y = (int) event.getRawY();
+    switch (event.getAction() & MotionEvent.ACTION_MASK) {
+      case MotionEvent.ACTION_DOWN:
+        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) getLayoutParams();
+        _xDelta = X - lParams.leftMargin;
+        _yDelta = Y - lParams.topMargin;
+        break;
+      case MotionEvent.ACTION_UP:
+        float dx = X - _xDelta;
+        float dy = Y - _yDelta;
+        callBack.move(component, dx, dy);
+        break;
+      case MotionEvent.ACTION_POINTER_DOWN:
+        break;
+      case MotionEvent.ACTION_POINTER_UP:
+        break;
+      case MotionEvent.ACTION_MOVE:
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) getLayoutParams();
+        layoutParams.leftMargin = X - _xDelta;
+        layoutParams.topMargin = Y - _yDelta;
+        layoutParams.rightMargin = -250;
+        layoutParams.bottomMargin = -250;
+        setLayoutParams(layoutParams);
+        break;
+    }
+    return true;
+  }
+};
