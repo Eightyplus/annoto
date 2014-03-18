c87f24ef9d956c630b8ae0f5c054e09718b3e3ac
Morten <friesgaard@gmail.com>
2014-03-18 17:42:33 +0100

    Picture component: added drawing capabilities upon matrix and serializable interface methods

--- src/dk/eightyplus/Painter/component/Picture.java
+++ src/dk/eightyplus/Painter/component/Picture.java
@@ -1,20 +1,26 @@
 package dk.eightyplus.Painter.component;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
- *
+ * Picture component in the Composite pattern
  */
 public class Picture extends Component {
 
   private float x = 0.0f;
   private float y = 0.0f;
-  private Bitmap bitmap;
   private float scale = 1.0f;
+  private transient Bitmap bitmap;
 
   public Picture(Bitmap bitmap) {
     this.bitmap = bitmap;
@@ -26,19 +32,21 @@ public class Picture extends Component {
 
   @Override
   public void onDraw(Canvas canvas, Paint paint) {
-    if (scale == 1.0f) {
-      canvas.drawBitmap(bitmap, x, y, paint);
-    } else {
-      canvas.drawBitmap(bitmap, getMatrix(), paint);
+    if (visible) {
+      if (scale == 1.0f) {
+        canvas.drawBitmap(bitmap, x, y, paint);
+      } else {
+        canvas.drawBitmap(bitmap, getMatrix(), paint);
+      }
     }
   }
 
-  // ERROR?
   private Matrix getMatrix() {
     float[] values = new float[Matrix.MPERSP_2 + 1];
     values[Matrix.MSCALE_X] = values[Matrix.MSCALE_Y] = scale;
     values[Matrix.MTRANS_X] = x;
     values[Matrix.MTRANS_Y] = y;
+    values[Matrix.MPERSP_2] = 1;
     Matrix matrix = new Matrix();
     matrix.reset();
     matrix.setValues(values);
@@ -58,6 +66,25 @@ public class Picture extends Component {
 
   @Override
   public RectF getBounds() {
-    return new RectF(x, y, x + bitmap.getWidth(), y + bitmap.getHeight());
+    return new RectF(x, y, x + bitmap.getWidth() * scale, y + bitmap.getHeight() * scale);
+  }
+
+  private void writeObject(ObjectOutputStream objectOutputStream) throws IOException {
+    objectOutputStream.defaultWriteObject();
+    if(bitmap != null){
+      ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+      boolean success = bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream);
+      if(success){
+        objectOutputStream.writeObject(byteStream.toByteArray());
+      }
+    }
+  }
+
+  private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException{
+    objectInputStream.defaultReadObject();
+    byte[] image = (byte[]) objectInputStream.readObject();
+    if(image != null && image.length > 0){
+      bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
+    }
   }
 }
