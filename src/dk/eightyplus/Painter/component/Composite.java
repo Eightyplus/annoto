b7c6540d0ae1ab58c001c361d911fd74507d3e4a
Morten Friesgaard Christensen <mfc@visiolink.com>
2014-08-19 12:22:24 +0200

    removed unused imports

--- src/dk/eightyplus/Painter/component/Composite.java
+++ src/dk/eightyplus/Painter/component/Composite.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.renderscript.Script;
 import dk.eightyplus.Painter.utilities.FileId;
 import dk.eightyplus.Painter.utilities.NoteStorage;
 import org.json.JSONException;
